1f53d6b4b9eeb96045a7c6cf74ce37e246d6af56
Riku S <63837480+risa562@users.noreply.github.com>
2023-11-27 13:20:02 +0200

    Add files via upload
    Double-checking GIT functionality.

---
+++ Testi3.py
@@ -0,0 +1,12 @@
+
+def example_function(mode):
+    print("This is a dummy function to test GIT functionality.")
+    if mode == 2:
+            print("ALARM!!!")
+            #play alert.waw here
+            #log alert date
+    elif mode == 3:
+            print("silent alarm...!")
+            #send alert notification
+    else:
+           print("This shouldn't be triggered.")
